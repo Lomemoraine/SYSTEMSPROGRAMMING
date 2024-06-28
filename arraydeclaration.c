a5f74add796027d362a497dc07dce69fa3a4a8e2
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2024-06-28 11:56:15 +0300

    add arraydeclaration

---
+++ arraydeclaration.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+ int arr[6] = {10,20,30,40,50};
+
+arr[2]= 60;
+int count;
+for (count = 0;count <6;count++){
+printf(" The elements of the array are %d\n",arr[count]);
+
+}
+return  0;
+
+
+
+
+
+
+
+}
